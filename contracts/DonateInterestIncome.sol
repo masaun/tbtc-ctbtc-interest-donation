2cc53a2c307f1b9d5d8028de2f54fe2bc8fc765e
masaun <masanoriuno@gmail.com>
2020-02-16 08:02:05 -0700

    Create 'cTBTC' instance by using /compound-protocol/CErc20.sol

--- contracts/DonateInterestIncome.sol
+++ contracts/DonateInterestIncome.sol
@@ -10,7 +10,7 @@ import "./storage/TbConstants.sol";
 import "@keep-network/tbtc/contracts/deposit/Deposit.sol";
 
 // Compound
-
+import "./compound-protocol/CErc20.sol";
 
 // Option (Candidate) list of Donation
 import "./CandidateListOfDonation.sol";
@@ -21,12 +21,36 @@ import "./CandidateListOfDonation.sol";
  ***/
 contract DonateInterestIncome is Ownable, TbStorage, TbConstants {
 
+    CErc20 public cErc20;
     Deposit public deposit;
     CandidateListOfDonation public candidateList;
 
-    constructor(address payable _depositAddress, address _candidateList) public {
+    address cTBTC = 0xb40d042a65dd413ae0fd85becf8d722e16bc46f1;  // cTBTC contractAddress on Ropsten
+
+    constructor(
+        address payable _depositAddress, 
+        address _candidateList,
+
+        // Compound
+        address cTBTC,
+        ComptrollerInterface comptroller_,
+        InterestRateModel interestRateModel_,
+        uint initialExchangeRateMantissa_,
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) public {
         deposit = Deposit(_depositAddress);
         candidateList = CandidateListOfDonation(_candidateList);
+        
+        // Create cTBTC instance
+        cErc20 = CErc20(cTBTC,
+                        comptroller_,
+                        interestRateModel_,
+                        initialExchangeRateMantissa_,
+                        name_,
+                        symbol_,
+                        decimals_);
     }
 
     function testFunc() public returns (bool) {
